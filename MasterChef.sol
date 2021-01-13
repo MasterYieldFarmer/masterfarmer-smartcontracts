098a57b2b137460f3a3e8f8241f79c69c6684fbe
nightg0at <nightg0at@protonmail.com>
2021-01-13 16:07:20 +0000

    implementation

--- MasterChef.sol
+++ MasterChef.sol
@@ -19,6 +19,7 @@ contract MasterChef is Ownable {
     struct UserInfo {
         uint256 amount;     // How many LP tokens the user has provided.
         uint256 rewardDebt; // Reward debt. See explanation below.
+        uint256 otherRewardDebt;
         //
         // We do some fancy math here. Basically, any point in time, the amount of CROPSs
         // entitled to a user but is pending to be distributed is:
@@ -38,6 +39,9 @@ contract MasterChef is Ownable {
         uint256 allocPoint;       // How many allocation points assigned to this pool. CROPSs to distribute per block.
         uint256 lastRewardBlock;  // Last block number that CROPSs distribution occurs.
         uint256 accCropsPerShare; // Accumulated CROPSs per share, times 1e12. See below.
+        uint256 accOtherPerShare; // Accumulated OTHERs per share, times 1e12. See below.
+        IStakingAdapter adapter; // Manages external farming
+        IERC20 otherToken; // The OTHER reward token for this pool, if any
     }
 
     // The CROPS TOKEN!
@@ -94,6 +98,14 @@ contract MasterChef is Ownable {
         startBlock = _startBlock;
     }
     
+    // rudimentary checks for the staking adapter
+    modifier validAdapter(IStakingAdapter _adapter) {
+        require(address(_adapter) != address(0), "no adapter specified");
+        require(_adapter.rewardTokenAddress() != address(0), "no other reward token specified in staking adapter");
+        require(_adapter.lpTokenAddress() != address(0), "no staking token specified in staking adapter");
+        _;
+    }
+
     function poolLength() external view returns (uint256) {
         return poolInfo.length;
     }
@@ -112,7 +124,34 @@ contract MasterChef is Ownable {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accCropsPerShare: 0
+            accCropsPerShare: 0,
+            accOtherPerShare: 0,
+            adapter: IStakingAdapter(0),
+            otherToken: IERC20(0)
+        }));
+    }
+
+    // Add a new lp to the pool that uses restaking. Can only be called by the owner.
+    function addWithRestaking(uint256 _allocPoint, bool _withUpdate, IStakingAdapter _adapter) public onlyOwner validAdapter(_adapter) {
+        IERC20 _lpToken = IERC20(_adapter.lpTokenAddress());
+
+        require(poolIsAdded[address(_lpToken)] == false, 'add: pool already added');
+        poolIsAdded[address(_lpToken)] = true;
+        
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: lastRewardBlock,
+            accCropsPerShare: 0,
+            accOtherPerShare: 0,
+            adapter: _adapter,
+            otherToken: IERC20(_adapter.rewardTokenAddress())
         }));
     }
 
@@ -125,6 +164,38 @@ contract MasterChef is Ownable {
         poolInfo[_pid].allocPoint = _allocPoint;
     }
 
+    // Set a new restaking adapter.
+    function setRestaking(uint256 _pid, IStakingAdapter _adapter, bool _claim) public onlyOwner validAdapter(_adapter) {
+        if (_claim) {
+            updatePool(_pid);
+        }
+        if (isRestaking(_pid)) {
+            withdrawRestakedLP(_pid);
+        }
+        PoolInfo storage pool = poolInfo[_pid];
+        require(address(pool.lpToken) == _adapter.lpTokenAddress(), "LP mismatch");
+        pool.accOtherPerShare = 0;
+        pool.adapter = _adapter;
+        pool.otherToken = IERC20(_adapter.rewardTokenAddress());
+
+        // transfer LPs to new target if we have any
+        uint256 poolBal = pool.lpToken.balanceOf(address(this));
+        if (poolBal > 0) {
+            pool.lpToken.safeTransfer(address(pool.adapter), poolBal);
+            pool.adapter.deposit(poolBal);
+        }
+    }
+
+    // remove restaking
+    function removeRestaking(uint256 _pid, bool _claim) public onlyOwner {
+        require(isRestaking(_pid), "not a restaking pool");
+        if (_claim) {
+            updatePool(_pid);
+        }
+        withdrawRestakedLP(_pid);
+        poolInfo[_pid].adapter = IStakingAdapter(address(0));
+        require(!isRestaking(_pid), "failed to remove restaking");
+    }
    
     // Return reward multiplier over the given _from to _to block.
     function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
@@ -153,6 +224,20 @@ contract MasterChef is Ownable {
         return user.amount.mul(accCropsPerShare).div(1e12).sub(user.rewardDebt);
     }
 
+    // View function to see our pending OTHERs on frontend (whatever the restaked reward token is)
+    function pendingOther(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accOtherPerShare = pool.accOtherPerShare;
+        uint256 lpSupply = pool.adapter.balance();
+ 
+        if (lpSupply != 0) {
+            uint256 otherReward = pool.adapter.pending();
+            accOtherPerShare = accOtherPerShare.add(otherReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accOtherPerShare).div(1e12).sub(user.otherRewardDebt);
+    }
+
     // Update reward vairables for all pools. Be careful of gas spending!
     function massUpdatePools() public {
         uint256 length = poolInfo.length;
@@ -172,11 +257,23 @@ contract MasterChef is Ownable {
         if (block.number <= pool.lastRewardBlock) {
             return;
         }
-        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+
+        uint256 lpSupply = getPoolSupply(_pid);
         if (lpSupply == 0) {
             pool.lastRewardBlock = block.number;
             return;
         }
+        if (isRestaking(_pid)) {
+            uint256 pendingOtherTokens = pool.adapter.pending();
+            if (pendingOtherTokens >= 0) {
+                uint256 otherBalanceBefore = pool.otherToken.balanceOf(address(this));
+                pool.adapter.claim();
+                uint256 otherBalanceAfter = pool.otherToken.balanceOf(address(this));
+                pendingOtherTokens = otherBalanceAfter.sub(otherBalanceBefore);
+                pool.accOtherPerShare = pool.accOtherPerShare.add(pendingOtherTokens.mul(1e12).div(lpSupply));
+            }
+        }
+
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
         uint256 cropsReward = multiplier.mul(cropsPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
         crops.mint(devaddr, cropsReward.div(10000).mul(teamMintrate));
@@ -185,18 +282,51 @@ contract MasterChef is Ownable {
         pool.lastRewardBlock = block.number;
     }
 
+    // Internal view function to get the amount of LP tokens staked in the specified pool
+    function getPoolSupply(uint256 _pid) internal view returns (uint256 lpSupply) {
+        PoolInfo memory pool = poolInfo[_pid];
+        if (isRestaking(_pid)) {
+            lpSupply = pool.adapter.balance();
+        } else {
+            lpSupply = pool.lpToken.balanceOf(address(this));
+        }
+    }
+
+    function isRestaking(uint256 _pid) public view returns (bool outcome) {
+        if (address(poolInfo[_pid].adapter) != address(0)) {
+            outcome = true;
+        } else {
+            outcome = false;
+        }
+    }
+
     // Deposit LP tokens to MasterChef for CROPS allocation.
     function deposit(uint256 _pid, uint256 _amount) public {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
+        uint256 otherPending = 0;
         if (user.amount > 0) {
             uint256 pending = user.amount.mul(pool.accCropsPerShare).div(1e12).sub(user.rewardDebt);
-            safeCropsTransfer(msg.sender, pending);
+            if(pending > 0) {
+                safeCropsTransfer(msg.sender, pending);
+            }
+            otherPending = user.amount.mul(pool.accOtherPerShare).div(1e12).sub(user.otherRewardDebt);
         }
-        pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
-        user.amount = user.amount.add(_amount);
+        if(_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            if (isRestaking(_pid)) {
+                pool.lpToken.safeTransfer(address(pool.adapter), _amount);
+                pool.adapter.deposit(_amount);
+            }
+            user.amount = user.amount.add(_amount);
+        }
+        // we can't guarantee we have the tokens until after adapter.deposit()
+        if (otherPending > 0) {
+            safeOtherTransfer(msg.sender, otherPending, _pid);
+        }
         user.rewardDebt = user.amount.mul(pool.accCropsPerShare).div(1e12);
+        user.otherRewardDebt = user.amount.mul(pool.accOtherPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
     
@@ -222,10 +352,23 @@ contract MasterChef is Ownable {
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
         uint256 pending = user.amount.mul(pool.accCropsPerShare).div(1e12).sub(user.rewardDebt);
-        safeCropsTransfer(msg.sender, pending);
-        user.amount = user.amount.sub(_amount);
+        if(pending > 0) {
+            safeCropsTransfer(msg.sender, pending);
+        }
+        uint256 otherPending = user.amount.mul(pool.accOtherPerShare).div(1e12).sub(user.otherRewardDebt);
+        if(_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            if (isRestaking(_pid)) {
+                pool.adapter.withdraw(_amount);
+            }
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        //  we can't guarantee we have the tokens until after adapter.withdraw()
+        if (otherPending > 0) {
+            safeOtherTransfer(msg.sender, otherPending, _pid);
+        }
         user.rewardDebt = user.amount.mul(pool.accCropsPerShare).div(1e12);
-        pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        user.otherRewardDebt = user.amount.mul(pool.accOtherPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -233,10 +376,25 @@ contract MasterChef is Ownable {
     function emergencyWithdraw(uint256 _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
-        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
-        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        uint256 amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
+        if (isRestaking(_pid)) {
+            pool.adapter.withdraw(amount);
+        }
+        pool.lpToken.safeTransfer(address(msg.sender), amount);
+        emit EmergencyWithdraw(msg.sender, _pid, amount);
+    }
+
+    // Withdraw LP tokens from the restaking target back here
+    // Does not claim rewards
+    function withdrawRestakedLP(uint256 _pid) internal {
+        require(isRestaking(_pid), "not a restaking pool");
+        PoolInfo storage pool = poolInfo[_pid];
+        uint lpBalanceBefore = pool.lpToken.balanceOf(address(this));
+        pool.adapter.emergencyWithdraw();
+        uint lpBalanceAfter = pool.lpToken.balanceOf(address(this));
+        emit EmergencyWithdraw(address(pool.adapter), _pid, lpBalanceAfter.sub(lpBalanceBefore));
     }
 
     // Safe crops transfer function, just in case if rounding error causes pool to not have enough CROPSs.
@@ -246,6 +404,17 @@ contract MasterChef is Ownable {
             crops.transfer(_to, cropsBal);
         } else {
             crops.transfer(_to, _amount);
+        }
+    }
+
+    // as above but for any restaking token
+    function safeOtherTransfer(address _to, uint256 _amount, uint256 _pid) internal {
+        PoolInfo storage pool = poolInfo[_pid];
+        uint256 otherBal = pool.otherToken.balanceOf(address(this));
+        if (_amount > otherBal) {
+            pool.otherToken.transfer(_to, otherBal);
+        } else {
+            pool.otherToken.transfer(_to, _amount);
         }
     }
 
